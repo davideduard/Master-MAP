a04cc910630cb895fd53b631f70ec1fc17653826
Eduard David <eduard.david0129@gmail.com>
2024-11-22 18:16:57 +0200

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace David_Eduard_Lab1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
